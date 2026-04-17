b76adb0e4e90a9ef6f08cdf1333253f5a6081db6
Md. Shah Alam <shahalam141072@gmail.com>
2026-04-17 17:57:32 +0600

    :bug: => Add HandleCors middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (MethodNotAllowedHttpException $e, $request) {
