17a527751b794c933db55268a3b5ebee3e6f28a0
Md. Shah Alam <shahalam141072@gmail.com>
2026-04-24 12:47:25 +0600

    :pencil2: => Introduce pusher and laravel event

---
+++ app/Events/SongCreated.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Song;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class SongCreated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $song;
+
+    public function __construct(Song $song)
+    {
+        Log::info('SongCreated Event Fired', [
+            'song_id' => $song->id,
+            'title' => $song->title
+        ]);
+        $this->song = $song;
+    }
+
+    public function broadcastOn()
+    {
+        return new Channel('admin-song-channel');
+    }
+
+    public function broadcastAs()
+    {
+        return 'song.created';
+    }
+}
